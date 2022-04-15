a21cc9ff52bea82bf8ff4900687a32a6bdf4bc9c
olliswe <oliveriyer@gmail.com>
2022-04-15 21:59:28 +0200

    wip

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,22 @@ import express, { Request, Response } from 'express';
 import axios from 'axios';
 import { JSDOM } from 'jsdom';
 import { Tabletojson } from 'tabletojson';
+import puppeteer from 'puppeteer'
 
+
+const launchPuppeteer = async (eventUrl:string) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(eventUrl);
+  await page.waitForSelector('.title');
+  await page.waitForSelector('select[name="playersTable_length"]');
+  await page.select('select[name="playersTable_length"]', '-1')
+  const names = await page.evaluate(() => Array.from(document?.querySelectorAll('.title')).map((node)=>node.textContent))
+  // const data = await page.evaluate(() => document?.querySelector('*')?.outerHTML);
+  // await page.screenshot({ path: 'example.png' });
+  await browser.close();
+  return names
+};
 const app = express();
 
 const getT3PlayerData = async ({ firstName, lastName }: { firstName: string; lastName: string }) => {
@@ -42,6 +57,43 @@ app.get('/', async (req: Request, res: Response) => {
   }
   res.send('Not found!');
 });
+
+app.get('/event', async (req: Request, res: Response) =>{
+  const eventUrl = req.query.link
+  if (typeof  eventUrl === 'string'){
+    const names = await launchPuppeteer(eventUrl)
+    const parsedNames = names.map((name)=>{
+      if(!name){
+        return {
+          firstName:'',
+          lastName:''
+        }
+      }
+      const splitString = name.trim().match(/^(\S+)\s(.*)/)?.slice(1)
+      if (splitString){
+        return {
+          firstName: splitString[0],
+          lastName: splitString[1]
+        }
+      }
+      return {
+        firstName:'',
+        lastName:''
+      }
+    })
+    const withNicknames = await Promise.all(parsedNames.map(async (name)=>{
+      const {nickname} = await getT3PlayerData(name)
+      return {
+        ...name,
+        nickname:nickname || 'unknown'
+      }
+    }))
+    res.send({success:true, names:withNicknames});
+    return
+  }
+  res.send({success:false})
+
+})
 
 app.listen(3000, () => {
   console.log('Application started on port 3000!');
