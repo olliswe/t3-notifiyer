5d778483ed3ce0bfff64a5ba2f002ad1a97e99ff
olliswe <oliveriyer@gmail.com>
2022-04-23 13:14:00 +0200

    error handling

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,11 @@ import axios from 'axios';
 import { JSDOM } from 'jsdom';
 import { Tabletojson } from 'tabletojson';
 import puppeteer from 'puppeteer'
+import cors from 'cors'
 
 
-const launchPuppeteer = async (eventUrl:string) => {
+
+const getBCPData = async (eventUrl:string) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(eventUrl);
@@ -13,12 +15,55 @@ const launchPuppeteer = async (eventUrl:string) => {
   await page.waitForSelector('select[name="playersTable_length"]');
   await page.select('select[name="playersTable_length"]', '-1')
   const names = await page.evaluate(() => Array.from(document?.querySelectorAll('.title')).map((node)=>node.textContent))
-  // const data = await page.evaluate(() => document?.querySelector('*')?.outerHTML);
-  // await page.screenshot({ path: 'example.png' });
+  const titles = await page.evaluate(() => Array.from(document?.querySelectorAll('.desc')).map((node)=>node.textContent))
+  const armiesAndTeam = titles.map((title)=>{
+    if (!title){
+      return {army:'unknown',team:'unknown'}
+    }
+    const titleArray = title.split('-')
+    if (titleArray.length === 0){
+      return {army:'unknown',team:'unknown'}
+    }
+    if (titleArray.length === 2) {
+      return {army:titleArray[0].trim(),team:titleArray[1].trim()}
+    }
+    return {army:titleArray[0].trim(),team:''}
+  })
+  const parsedResults = names.map((name, index)=>{
+    const army = armiesAndTeam[index].army
+    const team = armiesAndTeam[index].team
+    if(!name){
+      return {
+        firstName:'',
+        lastName:'',
+        army,
+        team
+      }
+    }
+    const splitString = name.trim().match(/^(\S+)\s(.*)/)?.slice(1)
+    if (splitString){
+      return {
+        firstName: splitString[0],
+        lastName: splitString[1],
+        army,
+        team
+      }
+    }
+    return {
+      firstName:'',
+      lastName:'',
+      army,
+      team
+    }
+  })
   await browser.close();
-  return names
+  return parsedResults
 };
+
+
 const app = express();
+app.use(cors())
+
 
 const getT3PlayerData = async ({ firstName, lastName }: { firstName: string; lastName: string }) => {
   const htmlString = await axios('https://www.tabletopturniere.de/t3_ntr_search.php', {
@@ -61,35 +106,22 @@ app.get('/', async (req: Request, res: Response) => {
 app.get('/event', async (req: Request, res: Response) =>{
   const eventUrl = req.query.link
   if (typeof  eventUrl === 'string'){
-    const names = await launchPuppeteer(eventUrl)
-    const parsedNames = names.map((name)=>{
-      if(!name){
+    try {
+      const bcpData = await getBCPData(eventUrl)
+      const withNicknames = await Promise.all(bcpData.map(async (name)=>{
+        const {nickname} = await getT3PlayerData(name)
         return {
-          firstName:'',
-          lastName:''
+          ...name,
+          nickname:nickname || 'unknown'
         }
-      }
-      const splitString = name.trim().match(/^(\S+)\s(.*)/)?.slice(1)
-      if (splitString){
-        return {
-          firstName: splitString[0],
-          lastName: splitString[1]
-        }
-      }
-      return {
-        firstName:'',
-        lastName:''
-      }
-    })
-    const withNicknames = await Promise.all(parsedNames.map(async (name)=>{
-      const {nickname} = await getT3PlayerData(name)
-      return {
-        ...name,
-        nickname:nickname || 'unknown'
-      }
-    }))
-    res.send({success:true, names:withNicknames});
-    return
+      }))
+      res.send({success:true, names:withNicknames});
+      return
+    } catch (error){
+      res.send({success:false})
+      return
+    }
+
   }
   res.send({success:false})
 
