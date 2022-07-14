611b9d9d50b419aabad2b5a1ce74ec6c21a6bf3b
olliswe <oliveriyer@gmail.com>
2022-07-14 18:03:12 +0200

    add signup

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
+import * as EmailValidator from 'email-validator';
 
 const prisma = new PrismaClient();
 
@@ -9,7 +10,25 @@ app.use(cors());
 
 app.get('/tournaments', async (req: Request, res: Response) => {
   const tournaments = await prisma.tournament.findMany();
-  res.send({ success: true, tournaments });
+  res.send({ tournaments });
+});
+
+app.post('/signup-email', async (req, res) => {
+  const email = req.body.email;
+  if (!email) {
+    res.status(403).send({ error: 'Email must be provided!' });
+    return;
+  }
+  if (!EmailValidator.validate(email)) {
+    res.status(403).send({ error: 'Email must be of valid format!' });
+    return;
+  }
+  await prisma.user.create({
+    data: {
+      email: email,
+    },
+  });
+  res.status(201).send();
 });
 
 app.listen(3000, () => {
